4629b0baad18498e09c3074b64da018a259a8621
Ccordova41 <ccordova41@gmail.com>
2018-05-25 21:26:19 -0400

    add imagemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,7 +16,7 @@ module.exports = function(grunt) {
           sizes: [{
             /*
             Change these:
-            
+
             width: ,
             suffix: ,
             quality:
@@ -53,6 +53,37 @@ module.exports = function(grunt) {
       },
     },
 
+    uglify: {
+        build: {
+            src: 'js/build/production.js',
+            dest: 'js/build/production.min.js'
+        }
+    },
+
+
+    imagemin: {
+        dynamic: {
+            files: [{
+                expand: true,
+                cwd: 'images/',
+                src: ['**/*.{png,jpg,gif}'],
+                dest: 'images/build/'
+              }, {
+
+                expand: true,
+                cwd: 'images_src/',
+                src: ['**/*.{png,jpg,gif}'],
+                dest: 'images/build/'
+
+
+
+
+            }]
+        }
+    },
+
+
+
     /* Copy the "fixed" images that don't go through processing into the images/directory */
     copy: {
       dev: {
@@ -64,11 +95,13 @@ module.exports = function(grunt) {
       },
     },
   });
-  
+
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  // grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-responsive-images');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-mkdir');
-  grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images']);
+  grunt.registerTask('default', ['clean', 'mkdir', 'copy', 'responsive_images', 'imagemin']);
 
 };
